c86e22d073327861c23f05edeff36c4ef0aa3c70
Carlosromero23 <carlosandres23072001@gmail.com>
2025-03-17 10:28:30 -0500

    me meti con genesis

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\katiana\Music\c\flask\3. app_empresa\my-app\env"
+set "VIRTUAL_ENV=C:\Users\katiana\Music\c\bavaria\APP_BAVARIA_SST\env"
 
 if not defined PROMPT set PROMPT=$P$G
 
